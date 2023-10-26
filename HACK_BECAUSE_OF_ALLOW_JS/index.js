dc868f3c1c5e13ebb0d687c54a935119c8c87310
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-10-26 15:10:51 +0300

    create index.js push

---
+++ HACK_BECAUSE_OF_ALLOW_JS/index.js
@@ -0,0 +1,40 @@
+// Import necessary libraries
+import * as express from 'express';
+// Create an Express application
+const app = express();
+const port = 3000; // Change this to your desired port
+// Middleware for parsing JSON in requests
+app.use(express.json());
+// Create a new vote
+app.post('/votes', (req, res) => {
+    try {
+        const vote = req.body;
+        // Add your validation and error handling here
+        // For demonstration purposes, we'll assume you have a mock database here.
+        // You can replace this with your actual database logic.
+        const mockDatabase = [];
+        // Add the new vote to the database
+        mockDatabase.push(vote);
+        res.status(201).json(vote); // Return the created vote
+    }
+    catch (error) {
+        res.status(500).send(error);
+    }
+});
+// Get all votes
+app.get('/votes', (req, res) => {
+    try {
+        // For demonstration purposes, we'll assume you have a mock database here.
+        // You can replace this with your actual database logic.
+        const mockDatabase = [];
+        // Return all votes from the database
+        res.json(mockDatabase);
+    }
+    catch (error) {
+        res.status(500).send(error);
+    }
+});
+// Start the Express server
+app.listen(port, () => {
+    console.log(`Voting app listening on port ${port}`);
+});
